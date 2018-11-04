d0ee906cb429b5e748c97b4f7d39c46d10cdc353
Vanita <vanitasharma_16@hotmail.com>
2018-11-04 00:54:40 -0700

    CHANGES

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -28,29 +28,101 @@ function start() {
     connection.query("SELECT * FROM products", function(err, res) {
       if (err) throw err;
       // Log all results of the SELECT statement
-      console.log(res);
-  inquirer 
-    .prompt({
-      name: "productID",
-      type: "rawlist",
-      message: "Which product would you like to buy?",
-      choices: ["Scarf", "Cardigan", "Dress", "Shirt", "Necklace,", "Earrings", "Rings", "Nail_Polish", "Nail_Polish_Remover", "Perfume"]
-    })
-    .then(function(answer) {
-    inquirer 
-    .prompt({
-        name: "Quantity",
-        type: "rawlist",
-        message: "How many units would you like to buy?",
-        choices: function() {
-            var choiceArray = [];
-            for (var i = 0; i < results.length; i++) {
-              choiceArray.push(results[i].stock_quantity);
-            }
-            return choiceArray;
-          },
-      });
+      console.table(res);
+
+      promtCustomerForItem(res);
     });
-connection.end();
-});
-}
+  }
+
+  function promtCustomerForItem (inventory) {
+  
+  inquirer 
+    .prompt([
+      {
+      name: "productID",
+      type: "input",
+      message: "Which product would you like to buy? [Quit with Q]",
+      validate: function(val) {
+        return !isNaN(val) || val.toLowerCase() === "q"; 
+      }
+      }
+    ])
+      // choices: ["Scarf", "Cardigan", "Dress", "Shirt", "Necklace,", "Earrings", "Rings", "Nail_Polish", "Nail_Polish_Remover", "Perfume"
+    .then(function(val) {
+      exit(val.productID);
+      var choiceID =
+      parseInt(val.productID);
+      var product =
+      checkInventory(choiceID, inventory);
+
+      if (product) {
+        quanitityPrompt(product); 
+      }
+      else {
+        console.log("\nThat item is not in stock.");
+        loadProducts();
+      }
+    });
+  }
+
+  function quanitityPrompt(product) {
+
+    inquirer 
+    .prompt([
+      {
+        name: "Quantity",
+        type: "input",
+        message: "How many units would you like to buy? [Quite with Q]",
+        validate: function(val) {
+          return cal > 0 ||
+          val.toLowerCase() === "q";
+        }
+        }
+      ])
+      .then(function(val) {
+        exit(val.Quanitity);
+        var Quanitity = parseInt(val.Quanitity);
+      if (Quanitity > product.stock_Quantity) {
+        console.log("\nInsufficient Quantity!");
+        loadProducts();
+      }
+      else {
+        makePurchase(product, Quanitity);
+      }
+    });
+  }
+
+  function makePurchase(product, Quanitity)
+  {
+    connection.query(
+      "UPDATE products SET ? WHERE ?",
+      [{stock_Quantity: Quanitity},
+      {item_id: product.item_id}],
+      function(err, res) {
+        console.log('RES AFTER PURCHASE:', res)
+        console.log("\nSuccessfully purchased " + Quanitity + " " + product.product_name + "'s!");
+        loadProducts();
+      }
+    );
+  }
+
+  function checkInventory(choiceID, inventory) {
+    for (var i = 0; i < inventory.length; i++) {
+      if (inventory[i].item_id === choiceId) {
+        // If a matching product is found, return the product
+        return inventory[i];
+      }
+    }
+    // Otherwise return null
+    return null;
+  }
+  
+  // Check to see if the user wants to quit the program
+  function checkIfShouldExit(choice) {
+    if (choice.toLowerCase() === "q") {
+      // Log a message and exit the current node process
+      console.log("Goodbye!");
+      process.exit(0);
+    }
+  }
+  
--- schema.sql
+++ schema.sql
@@ -9,42 +9,26 @@ USE bamazon_db;
 -- Creates the table "products" within bamazon_db --
 CREATE TABLE products (
 item_id INTEGER(11) AUTO_INCREMENT NOT NULL,
-product_name VARCHAR(30),
-department_name VARCHAR(30),
-price INT(10),
-stock_quantity INT(10),
+product_name VARCHAR(30)  NOT NULL,
+department_name VARCHAR(30)  NOT NULL,
+price DECIMAL(10,2)  NOT NULL,
+stock_quantity INT(10)  NOT NULL,
 PRIMARY KEY(item_id)
 );
+
+SELECT * FROM products;
 -- Creates new rows containing data in all named columns --
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Scarf", "Clothing", 20, 100);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Cardigan", "Clothing", 30, 200);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Dress", "Clothing", 50, 150);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Shirt", "Clothing", 25, 200);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Necklace", "Jewelry", 15, 40);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Earrings", "Jewelry", 10, 30);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Rings", "Jewelry", 5, 10);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Nail_Polish", "Cosmetics", 5, 15);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Nail_Polish_Remover", "Cosmetics", 5, 10);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Perfume", "Cosmetics", 50, 20); 
+VALUES ("Scarf", "Clothing", 20, 100),
+ ("Cardigan", "Clothing", 30, 200),
+("Dress", "Clothing", 50, 150),
+ ("Shirt", "Clothing", 25, 200),
+("Necklace", "Jewelry", 15, 40),
+("Earrings", "Jewelry", 10, 30),
+("Rings", "Jewelry", 5, 10),
+("Nail_Polish", "Cosmetics", 5, 15),
+("Nail_Polish_Remover", "Cosmetics", 5, 10),
+("Perfume", "Cosmetics", 50, 20);
 
 
 
